e97163988a5b13fc0f1d63ec8cf253100eb58343
Adrian Mowat <adrian.mowat@gmail.com>
2012-09-28 16:01:13 +0100

    Show non-unique patients only once

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -3,7 +3,10 @@ class Membership < ActiveRecord::Base
   belongs_to :patient
 
   validates :patient_id, :patient_list_id, :presence => true
-  validates :patient_id, :uniqueness => {:scope => :patient_list_id, :message => "is already on this list"}
+  validates :patient_id,
+    :uniqueness => {
+      :scope => :patient_list_id, :message => "is already on this list"
+    }
 
   def owner
     self.patient_list.user
