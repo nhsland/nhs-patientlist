7cc03b5ff39d99f5c48e44ecb66c31f74c249466
Mark Connell <mark@newcontext.com>
2012-10-22 13:14:25 +0100

    remove helpers that aren't being used.

--- app/helpers/patients_helper.rb
+++ app/helpers/patients_helper.rb
@@ -1,13 +1,7 @@
 module PatientsHelper
+
   def full_name(patient)
     "#{patient.firstnames} #{patient.lastname}"
   end
 
-  def back_link_text
-    ward ? "Back to #{ward}" : "Back to all patients"
-  end
-
-  def audit_status(status)
-     status.is_a?(Array) ? status[1] : status
-  end
 end
