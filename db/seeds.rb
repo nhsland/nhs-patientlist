affc60d2b227848992b4cf03e549adc95c83705d
Mark Connell <mark@newcontext.com>
2012-10-22 11:16:24 +0100

    clean up seeds file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,28 +4,6 @@ def with_header(header_message)
   puts ""
 end
 
-## Grades
-#
-with_header "Creating grades..." do
-  [
-    "Foundation Year 1",
-    "Foundation Year 2",
-    "Speciality Trainee 1",
-    "Speciality Trainee 2",
-    "Speciality Trainee 3",
-    "Speciality Trainee 4",
-    "Speciality Trainee 5",
-    "Speciality Trainee 6",
-    "Speciality Trainee 7",
-    "Speciality Trainee 8",
-    "Consultant"
-  ].each do |title|
-    puts "  #{title}"
-    g = Grade.find_or_create_by_title(title)
-    g.save
-  end
-end
-
 ## Shifts
 #
 with_header "Creating shifts..." do
