f3b6699a489b0195c733d0959b244d71a443a471
Mark Connell <mark@newcontext.com>
2012-10-23 16:43:01 +0100

    user has many team memberships, and therefore, many teams

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   has_many :patient_lists
+  has_many :team_memberships
+  has_many :teams, :through => :team_memberships
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,8 @@
 require 'spec_helper'
 
 describe User do
-  # add tests when we actually change something here
+
+  it { should have_many(:team_memberships) }
+  it { should have_many(:teams).through(:team_memberships) }
+
 end
