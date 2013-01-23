a24d72c49a6be45a229e56b31cbef17e451447ae
Daniel Grieve <daniel.grieve@neo.com>
2013-01-23 13:40:57 +0000

    add grades back in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
   has_many :patient_lists
+  belongs_to :grade
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :grade, :grade_id
 end
---
+++ db/seeds.rb
@@ -0,0 +1,15 @@
+[
+  "Foundation Year 1",
+  "Foundation Year 2",
+  "Speciality Trainee 1",
+  "Speciality Trainee 2",
+  "Speciality Trainee 3",
+  "Speciality Trainee 4",
+  "Speciality Trainee 5",
+  "Speciality Trainee 6",
+  "Speciality Trainee 7",
+  "Speciality Trainee 8",
+  "Consultant"
+].each do |title|
+  Grade.find_or_create_by_title(title)
+end
